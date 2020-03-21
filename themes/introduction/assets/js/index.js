f909864a9f4593ebe8f1653cb07e32f7ad2833c8
BjornW <burobjorn@burobjorn.nl>
2020-03-21 22:41:04 +0100

    Feature: Choose rnd Jitsi server
    First version of the Jitsi server room creator. It generates a random
    number and uses this as a room naam for the server. For now it uses
    vc4all.nl by default, but the next version will use the Servers list to
    also select a random server.
    
    TODO
    - Select server from the server list
    - Move the js code into a clean seperate js file instead of hacking the
    theme

--- themes/introduction/assets/js/index.js
+++ themes/introduction/assets/js/index.js
@@ -1,6 +1,25 @@
 // Nav burger animation
 document.addEventListener("DOMContentLoaded", function () {
-  // Get all "navbar-burger" elements
+
+// Start vc4all custom js 
+// todo need a way to load this from a seperate js file
+genRandomNumbers = function getRandomNumbers() {
+  var array = new Uint32Array(1);
+  var nrs_string = '';  
+  window.crypto.getRandomValues(array);
+  for (var i = 0; i < array.length; i++) {
+    nrs_string += array[i] + " ";
+  }
+  return nrs_string;  
+}
+
+var jitsi_server_url = document.querySelector("a.btn-start-call").getAttribute( 'href')
+var room_nr = genRandomNumbers(); 
+var jitsi_server_room = jitsi_server_url + '/' + room_nr; 
+document.querySelector("a.btn-start-call").setAttribute( 'href', jitsi_server_room);
+// End custom vc4all js
+
+ // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
@@ -43,3 +62,5 @@ $(document).keypress(function(e) {
         $("html").removeClass("modal-open");
     }
 });
+
+
